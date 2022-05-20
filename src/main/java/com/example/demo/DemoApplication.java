c72b5194b2d1b18887ddf7c556866830c9c09ed7
천광규 <kk.chon@tg360tech.com>
2022-05-20 12:41:14 +0900

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootApplication
-public class DemoApplication {
+@Slf4j
+public class DemoApplication implements ApplicationRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("ApplicationRunner");
+	}
 }
